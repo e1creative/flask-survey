62cba7f6f5ab0ba20b680d95eecc7215c331259d
Jason Takahashi <jasontakahashi@Jasons-MacBook-Pro.local>
2022-08-27 21:39:40 -0700

    Finish exercise up to step 7.

---
+++ app.py
@@ -0,0 +1,75 @@
+from urllib import response
+from flask import Flask, render_template, request, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+
+from surveys import satisfaction_survey 
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "testingflask"
+debug = DebugToolbarExtension(app)
+
+
+responses = []
+
+@app.route("/")
+def home():
+    """Show our home page with title, instructions and start button"""
+
+    print(f"responses : {responses}")
+
+    return render_template("home.html", title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
+
+
+@app.route("/questions/<int:question_num>")
+def questions(question_num):
+    """Show our question based on the url string"""
+
+    print(f"responses : {responses}")
+
+    referrer = request.referrer
+    print(f"referrer url: {referrer}")
+
+
+    # if there is no referrer (manually entered a url)
+    if not referrer:
+        # if we already have 4 responses, send them to the thank you page
+        if len(responses) == 4:
+            flash("You already have 4 responses")
+            return redirect("/thankyou")
+
+        # if we don't have 4 responses, send them to the next question page
+        send_back_to_question_num = str(len(responses)+1)
+        send_back_to_question_path = f"/questions/{send_back_to_question_num}"
+        flash("Please answer the questions in order")
+        return redirect(send_back_to_question_path)
+
+    # the index of the question will be current_question-1
+    current_question = satisfaction_survey.questions[question_num-1].question
+    current_choices = satisfaction_survey.questions[question_num-1].choices
+
+    return render_template("questions.html", title=satisfaction_survey.title, current_question=current_question, current_choices=current_choices, question_num=question_num)
+
+
+@app.route("/answer", methods=["POST"])
+def answers():
+    """Add answer to our responses list then redirect customer to the next question OR to a thank you page"""
+
+    responses.append(request.form.get('answer'))
+
+    # current_question_num will be a string, we need to convert to an int to add, then convert back to string for the path
+    current_question = int(request.form.get('current_question_num'))
+
+    if current_question >= len(satisfaction_survey.questions):
+        return redirect("/thankyou")
+    
+    next_question = str(current_question + 1)
+        
+    next_path = f"/questions/{next_question}"
+    
+    return redirect(next_path)
+
+
+@app.route("/thankyou")
+def thank_you():
+    return render_template("thankyou.html", responses=responses)
