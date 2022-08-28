3f3789236eaa80f447b7bcf1b55780158bfbb93e
Jason Takahashi <jasontakahashi@Jasons-MacBook-Pro.local>
2022-08-28 00:23:33 -0700

    Add session support.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from crypt import methods
 from urllib import response
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from surveys import satisfaction_survey 
@@ -10,22 +11,32 @@ app.config['SECRET_KEY'] = "testingflask"
 debug = DebugToolbarExtension(app)
 
 
-responses = []
-
 @app.route("/")
 def home():
     """Show our home page with title, instructions and start button"""
 
-    print(f"responses : {responses}")
-
     return render_template("home.html", title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
+
+
+@app.route("/", methods=["POST"])
+def home_sessions():
+    """Reset session"""
+    session["responses"] = []
+
+    print("*********** session ***********")
+    print(session["responses"])
+    print("----------- session -----------")
+
+    return redirect("/questions/1")
 
 
 @app.route("/questions/<int:question_num>")
 def questions(question_num):
     """Show our question based on the url string"""
 
-    print(f"responses : {responses}")
+    print("*********** session ***********")
+    print(session["responses"])
+    print("----------- session -----------")
 
     referrer = request.referrer
     print(f"referrer url: {referrer}")
@@ -34,12 +45,12 @@ def questions(question_num):
     # if there is no referrer (manually entered a url)
     if not referrer:
         # if we already have 4 responses, send them to the thank you page
-        if len(responses) == 4:
+        if len(session["responses"]) == 4:
             flash("You already have 4 responses")
             return redirect("/thankyou")
 
         # if we don't have 4 responses, send them to the next question page
-        send_back_to_question_num = str(len(responses)+1)
+        send_back_to_question_num = str(len(session["responses"])+1)
         send_back_to_question_path = f"/questions/{send_back_to_question_num}"
         flash("Please answer the questions in order")
         return redirect(send_back_to_question_path)
@@ -55,7 +66,9 @@ def questions(question_num):
 def answers():
     """Add answer to our responses list then redirect customer to the next question OR to a thank you page"""
 
+    responses = session["responses"]
     responses.append(request.form.get('answer'))
+    session["responses"] = responses
 
     # current_question_num will be a string, we need to convert to an int to add, then convert back to string for the path
     current_question = int(request.form.get('current_question_num'))
@@ -72,4 +85,4 @@ def answers():
 
 @app.route("/thankyou")
 def thank_you():
-    return render_template("thankyou.html", responses=responses)
+    return render_template("thankyou.html")
